835ee88413fd70b0e7f7bcbc74808e1b1be4b7b1
dr-data <data@graduate.hku.hk>
2024-07-15 00:35:33 +0800

    Revert "link first"
    This reverts commit 3482f2b26cfe1c8d73969da697d20b4fd24e884e.

--- server/api/link/edit.put.ts
+++ server/api/link/edit.put.ts
@@ -1,13 +1,13 @@
 import { LinkSchema } from '@/schemas/link'
 
 export default eventHandler(async (event) => {
-  const { oldSlug, newSlug, ...linkData } = await readValidatedBody(event, LinkSchema.parse)
+  const link = await readValidatedBody(event, LinkSchema.parse)
 
   const { cloudflare } = event.context
   const { KV } = cloudflare.env
 
   // Fetch the existing link data using the old slug
-  const existingLink = await KV.get(`link:${oldSlug}`)
+  const existingLink = await KV.get(`link:${link.oldSlug}`)
   if (!existingLink) {
     throw createError({
       status: 404, // Not Found
@@ -15,11 +15,11 @@ export default eventHandler(async (event) => {
     })
   }
 
-  // Determine the updated slug
-  const updatedSlug = newSlug ? modifySlug(newSlug) : oldSlug
-
+  // Modify the slug if it is being updated
+  const updatedSlug = link.newSlug ? modifySlug(link.newSlug) : link.oldSlug
+  
   // Check for slug conflict if the slug is updated
-  if (newSlug) {
+  if (link.newSlug) {
     const conflictLink = await KV.get(`link:${updatedSlug}`)
     if (conflictLink) {
       throw createError({
@@ -30,20 +30,20 @@ export default eventHandler(async (event) => {
   }
 
   // Save the updated link with the new slug
-  await KV.put(`link:${updatedSlug}`, JSON.stringify({ ...JSON.parse(existingLink), ...linkData, slug: updatedSlug }), {
-    expiration: linkData.expiration,
+  await KV.put(`link:${updatedSlug}`, JSON.stringify({ ...link, slug: updatedSlug }), {
+    expiration: link.expiration,
     metadata: {
-      expiration: linkData.expiration,
+      expiration: link.expiration,
     },
   })
 
   // Delete the old link if the slug has changed
-  if (oldSlug !== updatedSlug) {
-    await KV.delete(`link:${oldSlug}`)
+  if (link.oldSlug !== updatedSlug) {
+    await KV.delete(`link:${link.oldSlug}`)
   }
 
   setResponseStatus(event, 200)
-  return { link: { ...JSON.parse(existingLink), ...linkData, slug: updatedSlug } }
+  return { link: { ...link, slug: updatedSlug } }
 })
 
 // Function to modify the slug
